fdaca113b3010cce7cd44de4390ec901d45d9c9e
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-06-22 17:59:05 +0200

    Update Showdown and add Style to code in markdown

--- example/script.js
+++ example/script.js
@@ -1,6 +1,5 @@
 // Show the pressed page and hide others
 function showContent(contentId) {
-    // Page information
     var contents = document.getElementsByClassName("wikiContent");
     for (var i = 0; i < contents.length; i++) {
         contents[i].style.display = "none";
@@ -16,16 +15,11 @@ function showContent(contentId) {
 function getContent(page) {
     fetch('content/' + page + '.md')
         .then(function(response) {
-          return response.text();
+            return response.text();
         })
         .then(function(markdownContent) {
-          // Crear el convertidor de Markdown a HTML
-          var converter = new showdown.Converter();
-
-          // Convertir el contenido Markdown en HTML
-          var htmlContent = converter.makeHtml(markdownContent);
-
-          // Insertar el HTML convertido en el contenedor
-          document.getElementById(page + '-markdown').innerHTML = htmlContent;
+            var converter = new showdown.Converter();
+            var htmlContent = converter.makeHtml(markdownContent);
+            document.getElementById(page + '-markdown').innerHTML = htmlContent;
         });
 }
